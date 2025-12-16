f5b3b3f18122d5df58481c2be4fc18015b5e827b
sm-remal <remal.mozzem@gmail.com>
2025-12-16 12:01:06 +0600

    update axios secure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,52 +4,43 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000'
-})
+    baseURL: "http://localhost:3000"
+});
 
 const useAxiosSecure = () => {
-
     const { user, signOutUser } = useAuth();
-    const navigate = useNavigate()
-    // set token in the header for all the API call using axiosSecure hook
+    const navigate = useNavigate();
 
     useEffect(() => {
-
         // Request Interceptor
-        const requestInterceptor = instance.interceptors.request.use((config) => {
-            // console.log(config)
-            const token = user.accessToken;
+        const requestInterceptor = instance.interceptors.request.use(config => {
+            const token = user?.accessToken;
             if (token) {
-                config.headers.authorization = `Bearer ${token}`
+                config.headers.authorization = `Bearer ${token}`;
             }
             return config;
-        })
+        });
 
         // Response Interceptor
-        const responseInterceptor = instance.interceptors.response.use(res => {
-            return res;
-        }, err => {
-            const status = err.status;
-            if (status === 401 || status === 403) {
-                // console.log("Log out the user for bad request");
-                signOutUser()
-                    .then(() => {
-                        navigate("/login");
-                    })
+        const responseInterceptor = instance.interceptors.response.use(
+            res => res,
+            err => {
+                const status = err.response?.status;
+                if (status === 401 || status === 403) {
+                    signOutUser().then(() => navigate("/login"));
+                }
+                return Promise.reject(err);
             }
-        })
-
+        );
 
         return () => {
             instance.interceptors.request.eject(requestInterceptor);
             instance.interceptors.response.eject(responseInterceptor);
-        }
-
-    }, [user, signOutUser, navigate])
+        };
+    }, [user, signOutUser, navigate]);
 
     return instance;
-}
+};
+
 export default useAxiosSecure;
-
-
 
